a9458ea36594860fa0f53992c101cb7874c5177f
Bosi Chen <chenbosi.bosi@gmail.com>
2021-11-25 02:33:33 -0500

    make modifications to avoid SQL injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,16 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+
+const queryString = `
 SELECT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name = 'JUL02'
+WHERE cohorts.name LIKE $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name;
-`)
+`
+const cohortName = process.argv[2];
+const value = [`%${cohortName}%`];
+pool.query(queryString, value)
 .then(res => {
   // console.log(res.rows);
   res.rows.forEach(user => {
